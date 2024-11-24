1267204ab78ad3dc267127f7d0b2cc99f2b7cf85
Yorick Bijlsma <yorick.bijlsma@hva.nl>
2024-11-24 01:04:32 +0100

    Add unit testing project

---
+++ ImageSteganography/UnitTesting/Controllers/ImageSteganographyController_Tests.cs
@@ -0,0 +1,27 @@
+﻿using ImageSteganography.Controllers;
+using ImageSteganography.Services;
+using Microsoft.Extensions.Configuration;
+using Moq;
+
+namespace UnitTesting.Controllers
+{
+	public class ImageSteganographyController_Tests																																					   
+	{
+		[Fact]
+		public void ImageSteganographyController_EncodesMessageCorrectly()
+		{
+			var jsonString = "{\"AllowedFileTypes\" : [\".jpg\", \".jpeg\", \".png\"]}";
+
+			var configuration = new ConfigurationBuilder()
+				.AddJsonStream(new MemoryStream(System.Text.Encoding.UTF8.GetBytes(jsonString)))
+				.Build();
+
+            var service = new Mock<ImageSteganographyService>(configuration);
+			var controller = new ImageSteganographyController(service.Object);
+
+			//TODO: act
+
+			Assert.Equal("", "");
+		}
+	}
+}
